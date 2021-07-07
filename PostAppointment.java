637e806fc643d98032a6cce9833ffbdde2788023
madsaintis <83106614+madsaintis@users.noreply.github.com>
2021-07-08 02:55:37 +0800

    Update PostAppointment.java

--- PostAppointment.java
+++ PostAppointment.java
@@ -1,15 +1,30 @@
-class PostAppointment {
-private float discount;
-private int count;
+class PostAppointment extends Appointment{
 
-public PostAppointment();
-public void toString() {
+private double discount;
 
-        System.out.println("Total price: ");
-        System.out.println("Discount: ");
-        System.out.println("Price after discount: ");
+public PostAppointment(Patient patient, String date, String time) {
+	super(patient,date, time);
+	getDiscount();
+	calculateCharge();
+}
+
+public void getDiscount() {
+	discount = followUpCharge * (followUpDiscountPerVisit * patient.getVisitCount());
+	
+}
+
+public void displayAppointment() {
+		System.out.println("--Appointment details--");
+		System.out.println("Patient's name   : " + patient.getName());
+        System.out.println("Doctor in charge : " + patient.getDoctor().getName());
+		System.out.println("Date             : " + date);
+		System.out.println("Time             : " + time);
+        System.out.printf("\nDiscount: RM%.2f\n", discount);
+        System.out.printf("Price after discount: RM%.2f\n\n", totalCharge);
 }
 
 public void calculateCharge() {
+	totalCharge = followUpCharge - discount;
 }
+
 }
