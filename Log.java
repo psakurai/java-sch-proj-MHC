c15c2a8436e9438bdb23cf19c6820a50cd2c1365
madsaintis <83106614+madsaintis@users.noreply.github.com>
2021-07-08 02:46:45 +0800

    Update Log.java

--- Log.java
+++ Log.java
@@ -1,12 +1,33 @@
-class Log {
-private String data;
-private Appointment appointment;
+import java.util.ArrayList;
 
-public Log();
+class Log {
+private ArrayList <Appointment>appointments;
+
+public Log()
+{
+        appointments = new ArrayList<>();
+}
+
 public void addAppointment(Appointment appointment) {
-        this.appointment = appointment;
+        appointments.add(appointment);
 }
-public void display() {
-        return data;
+
+public void displayList() {
+	
+	System.out.println("--List of appointments--");
+        System.out.println("\nNo\tPatient Name\t\tDate\tDoctor");
+        for(int i = 0; i < appointments.size(); i++)
+        {
+		System.out.printf("%d  %-30s %-15s %-20s",(i+1),appointments.get(i).getPatient().getName(),appointments.get(i).getDate(), appointments.get(i).getPatient().getDoctor().getName());
+		System.out.println();
+        }
+
+}	
+
+public void displayAppointmentDetails(int choice) {
+	System.out.println("Here is the details for Appointment " + (choice));
+	appointments.get(choice-1).displayAppointment();
 }
+
+
 }
