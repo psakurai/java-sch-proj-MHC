25e5085eb864f74133fe59ceb150a23b9fe71374
mrperfectt <77198649+mrperfectt@users.noreply.github.com>
2021-07-07 16:38:38 +0800

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -3,9 +3,20 @@ private String doctorName;
 private int contact;
 private String department;
 
-public Doctor();
-public void display() {
+public Doctor(String doctorName, int contact, String department)
+{
+    this.doctorName=doctorName;
+    this.contact=contact;
+    this.department=department;
+}
 
+
+public void display() 
+{
+    System.out.println("***************DOCTOR's DETAILS***************");
+    System.out.println("Name: " + doctorName);
+    System.out.println("Contact: " + contact);
+    System.out.println("Department: " + department);
 }
 }
-s
+
