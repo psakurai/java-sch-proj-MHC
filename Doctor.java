9043ab7a7fa5275ece06375b7578711a1d6d3896
madsaintis <83106614+madsaintis@users.noreply.github.com>
2021-07-08 02:48:11 +0800

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,11 +1,32 @@
 class Doctor {
 private String doctorName;
-private int contact;
+private String contact;
 private String department;
 
-public Doctor();
-public void display() {
+public Doctor(String doctorName, String contact, String department) {
+	this.doctorName = doctorName;
+	this.contact = contact;
+	this.department = department;
+	}
 
+public String getName() {
+	return doctorName;
+	}
+
+public String getContact() {
+		return contact;
+	}
+
+public String getDepartment() {
+		return department;
+	}
+	
+public void displayInfo() {
+	System.out.println("\n--Doctor Details--\n");
+	System.out.println("Name: " + doctorName);
+	System.out.println("Contact Number: " + contact);
+	System.out.println("Department: " + department);
+
+	}
 }
-}
-s
+
